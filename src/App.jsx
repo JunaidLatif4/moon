dc646948e485006e9e80302ce0ad68b974a8c145
ZahidGhafoor <zahidghafoor40@gmail.com>
2021-11-24 23:16:06 +0500

    token

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 import "./App.scss";
-import Calculator from "./components/calculator";
+// import Calculator from "./components/calculator";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Token from "./Components/Token/Token";
 
 const App = () => {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/" component={Calculator} />
+          {/* <Route path="/" component={Calculator} /> */}
+          <Route path="/token" component={Token} />
         </Switch>
       </Router>
     </>
---
+++ src/Components/Token/Token.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Token.scss"
+
+const Token = () => {
+    return (
+        <div className="token__container">
+            <div className="token__header">
+                <div className="heading">THE TAKION TOKEN</div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Token
