bfd85a5419f97a810fe380f337960f1cf0a509af
Junaid Latif <junaidlatif004@gmail.com>
2021-11-24 19:51:52 +0500

    MoonApp Initieated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "App.scss"
+
+const App = () => {
+    return (
+        <>
+            <div className="app_container">
+                This is React App , The <span style={{ fontFamily: "sansation" }} > Sansation Font . </span>
+            </div>
+        </>
+    )
+}
+
+export default App
