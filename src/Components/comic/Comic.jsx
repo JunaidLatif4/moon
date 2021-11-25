7b53cc3abd248ca404af7fb67c8fe4aae1931d6e
abdulqayyum686 <qayyuma686@gmail.com>
2021-11-25 19:31:53 +0500

    get footer

--- src/Components/comic/Comic.jsx
+++ src/Components/comic/Comic.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import "./comic.scss";
+import part1 from "../../assests/comic/part-1.png";
+import part2 from "../../assests/comic/part2.png";
+import part3 from "../../assests/comic/part3.png";
+import part4 from "../../assests/comic/part4.png";
 const Comic = () => {
   return (
     <>
@@ -7,10 +11,77 @@ const Comic = () => {
         <div className="comic_section">
           <h2 className="heading_style">ONE COMIC,</h2>
           <h2 className="heading_style">ONE EPIC SPACE ODISSEY</h2>
-          <p className="comic_paragraph">AN ACTION PACKED SPACE JOURNEY THAT WILL REIGNITE YOUR LOVE FOR COMIC BOOKS</p>
-          <p className="comic_paragraph">GET ABSORBED INTO A WORLD OF MYSTERY, FANTASY, AND UNLIMITED ACTION!</p>
+          <div className="d-flex justify-content-center">
+            <p className="comic_paragraph">
+              AN ACTION PACKED SPACE JOURNEY THAT WILL REIGNITE YOUR LOVE FOR
+              COMIC BOOKS
+            </p>
+          </div>
+          <p className="comic_getabsorbed">
+            GET ABSORBED INTO A WORLD OF MYSTERY, FANTASY, AND UNLIMITED ACTION!
+          </p>
         </div>
-        <div className="heading_style">hlhh</div>
+
+        <div className="story_section">
+          <h2 className="story_heading_style">THE STORYLINE</h2>
+          <div className="text-center">
+            <button className="story_comic_button">READ STORYLINE</button>
+          </div>
+        </div>
+
+        <div className="parts_section ">
+          <div className="nft_part1_style">
+            <div>
+              <img src={part1} alt="" />
+            </div>
+            {/* <div className="part1_footer">
+              <h3>PART 1:</h3>
+              <br />
+              DARK DAYS
+            </div> */}
+          </div>
+          <div className="nft_part1_style">
+            <div>
+              <img src={part2} alt="" />
+            </div>
+            {/* <div className="part1_footer">
+              <h3>PART 1:</h3>
+              <br />
+              DARK DAYS
+            </div> */}
+          </div>
+          <div className="nft_part1_style">
+            <div>
+              <img src={part3} alt="" />
+            </div>
+            {/* <div className="part1_footer">
+              <h3>PART 1:</h3>
+              <br />
+              DARK DAYS
+            </div> */}
+          </div>
+          <div className="nft_part1_style">
+            <div>
+              <img src={part4} alt="" />
+            </div>
+            {/* <div className="part1_footer">
+              <h3>PART 1:</h3>
+              <br />
+              DARK DAYS
+            </div> */}
+          </div>
+        </div>
+
+
+        <div className="nft_section">
+          <h2 className="heading_style">OWN AN NFT PAGE OF THE COMIC</h2>
+          <div className="d-flex justify-content-center">
+            <p className="nft_paragraph">
+            Owning a page, or even better, a full comic book, will allow you to receive special surprises, bonuses, and rewards!
+            </p>
+          </div>
+        
+        </div>
       </div>
     </>
   );
