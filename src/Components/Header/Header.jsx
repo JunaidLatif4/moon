3d316d0b85bb25bf7e590260af74c0b55dc95bd2
Junaid Latif <junaidlatif004@gmail.com>
2021-11-25 16:56:28 +0500

    HeaderComponent COMPLETED

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from "react-router-dom"
+
+import { AiOutlineMenu } from "react-icons/ai"
+
+import Logo from "../../assests/NavBar/logo.png"
+import Send from "../../assests/NavBar/send.png"
+import Twitter from "../../assests/NavBar/twitter.png"
+import Dis from "../../assests/NavBar/dis.png"
 
 import "./Header.scss"
 
 const Header = () => {
+
+    const [showDrop, setShowDrop] = useState(false)
+    const [changeNavbar, setChangeNavBar] = useState(false);
+    const changingNavBar = () => {
+        if (window.scrollY >= 80) {
+            setChangeNavBar(true);
+        }
+        else {
+            setChangeNavBar(false);
+        }
+    };
+    window.addEventListener('scroll', changingNavBar);
+
     return (
         <>
-            <div className="header_container">
+            <div className={`header_container ${changeNavbar ? "scroll_header" : null}`}>
                 <div className="box">
                     <nav>
-                        <NavLink to="/"> Home </NavLink>
+                        <NavLink to="/"> HOME </NavLink>
                         <NavLink to="/"> BUY NOW </NavLink>
                         <NavLink to="/"> TAK TOKEM </NavLink>
-                        <NavLink to="/"> Home </NavLink>
+                        <div className="learn"> <a href=""> LEARN MORE </a>
+                            <div className="learn_drop">
+                                <NavLink to="/"> ROADMAP </NavLink>
+                                <NavLink to="/"> MEMBERSHIP </NavLink>
+                                <NavLink to="/"> TEAM </NavLink>
+                                <NavLink to="/"> FAQS </NavLink>
+                                <NavLink to="/"> DOCS </NavLink>
+
+                            </div>
+                        </div>
                     </nav>
                     <div className="img_box">
-
+                        <img src={Logo} alt="ERROR" className="logo" />
+                        <div className="social_box">
+                            <img src={Send} alt="ERROR" />
+                            <img src={Dis} alt="ERROR" />
+                            <img src={Twitter} alt="ERROR" />
+                        </div>
                     </div>
                     <nav>
                         <NavLink to="/"> EVOLUTION LAB </NavLink>
@@ -24,6 +58,35 @@ const Header = () => {
                         <NavLink to="/"> ATTRIBUTES </NavLink>
                     </nav>
                 </div>
+
+                <div className="mbl_box">
+                    <img src={Logo} alt="ERROR" className="logo" />
+                    <div className="social_box">
+                        <img src={Send} alt="ERROR" />
+                        <img src={Dis} alt="ERROR" />
+                        <img src={Twitter} alt="ERROR" />
+                    </div>
+                    <div className="menu" onClick={() => setShowDrop(showDrop ? false : true)}>
+                        <AiOutlineMenu className="icon" />
+                    </div>
+
+                    <div className={`mbl_drop ${showDrop ? "show_drop" : null}`}>
+                        <div className="drop_box">
+                            <NavLink to="/"> HOME </NavLink>
+                            <NavLink to="/"> BUY NOW </NavLink>
+                            <NavLink to="/"> TAK TOKEN </NavLink>
+                            <NavLink to="/"> LEARN MORE </NavLink>
+                            <NavLink to="/" className="sub"> ROADMAP </NavLink>
+                            <NavLink to="/" className="sub"> MEMBERSHIP </NavLink>
+                            <NavLink to="/" className="sub"> TEAM </NavLink>
+                            <NavLink to="/" className="sub"> FAQS </NavLink>
+                            <NavLink to="/"> EVOLUTION LAB </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="free">
+                sdasd
             </div>
         </>
     )
