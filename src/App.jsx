320ce36a385342e6ff12d45899b50bc6ffda7c64
Muhammad590 <muhammadsulamanmalik@gmail.com>
2021-11-27 01:03:32 +0500

    for evolution tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Calculator from "./Components/calculator/calculator";
 import Header from "./Components/Header/Header";
 import Token from "./Components/Token/Token";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
+import Evaluation from "./Components/EvaluationLab/Evaluation";
 
 import "./App.scss";
-import Evaluation from "./Components/EvaluationLab/Evaluation";
 
 const App = () => {
   return (
--- src/Components/EvaluationLab/Evaluation.js
+++ src/Components/EvaluationLab/Evaluation.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Row, Col } from 'react-bootstrap'
 import monkey from "../../assests/Lab/monky1.png"
 import question from "../../assests/Lab/question.png"
-
-
-
-import "./Evaluation.css"
+import border from "../../assests/Lab/border.png"
+import sidearrow from "../../assests/Lab/sidearrow.png"
+import { IoIosArrowForward } from "react-icons/io"
+import "./Evaluation.scss"
 
 function Evaluation() {
+    const [active, setActive] = useState("2")
     return (
         <>
             <div>
@@ -70,42 +72,196 @@ function Evaluation() {
                         </div>
 
                         <div className="active_stageButton">
-                            <button className="active_stage2">Stage 2</button>
-                            <button className="active_stage22">Stage 3</button>
-                            <button className="active_stage22">Stage 4</button>
+                            <button className={`active_stage2 ${active == "2" && "activeBtn"}`} onClick={() => setActive("2")}>Stage 2</button>
+                            <button className={`active_stage22 ${active == "3" && "activeBtn"}`} onClick={() => setActive("3")}>Stage 3</button>
+                            <button className={`active_stage22 ${active == "4" && "activeBtn"}`} onClick={() => setActive("4")}>Stage 4</button>
                         </div>
+                        {active === "2" &&
+                            <div className="box_mainDiv">
+                                <div className="box_textDiv">
+                                    <div className="button_divMa">
+                                        <div>
+                                            <h4 className="box_nftText">My NFT Mooning Monkeys</h4>
+                                        </div>
+                                        <div className="both_button">
+                                            <div className="buttons_container">
+                                                <button> CONNECT WALLET <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
 
-                        <div className="box_mainDiv">
-                            <div className="box_textDiv">
-                                <h4 className="box_nftText">My NFT Mooning Monkeys</h4>
+                                            <div className="buttons_container">
+                                                <button> BUY MONKEY <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+                                        </div>
+
+                                    </div>
 
 
 
-                                <div className="first_textDiv">
-                                    <p className="order_text">In order to achieve the powerful <span className="gal">Galactic Gorilla</span>, you’ll have to sacrifice 4 of your current <span className="gal">Mooning Monkeys</span> to depart into deep space and surpass
-                                        the obstacles they’ll encounter on their way to the new planet.</p>
-                                    <p className="note_text"><span className="gal">NOTE:</span> By clicking “<span className="gal">Evolve</span>”, the <span>Mooning Monkeys</span> you’ve chosen will be sent to a Solana
-                                        black hole wallet and disappear forever, however,
-                                        <span>EACH EVOLUTION</span> will make you more money in the <span className="gal">Mooning Monkey Game.</span></p>
+                                    <div className="first_textDiv">
+                                        <p className="order_text">In order to achieve the powerful <span className="gal">Galactic Gorilla</span>, you’ll have to sacrifice 4 of your current <span className="gal">Mooning Monkeys</span> to depart into deep space and surpass
+                                            the obstacles they’ll encounter on their way to the new planet.</p>
+                                        <p className="note_text"><span className="gal">NOTE:</span> By clicking “<span className="gal">Evolve</span>”, the <span>Mooning Monkeys</span> you’ve chosen will be sent to a Solana
+                                            black hole wallet and disappear forever, however,
+                                            <span>EACH EVOLUTION</span> will make you more money in the <span className="gal">Mooning Monkey Game.</span></p>
+                                    </div>
+
+
+                                    <div className="evolve_process">
+                                        <div className="border_main_div">
+                                            <h4 className="strt_evolve">Start evolve process</h4>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="first_round_image">
+                                            <img className="border_img" src={border} />
+                                            <img className="border_img ones" src={border} />
+                                            <img className="border_img ones" src={border} />
+                                            <img className="border_img ones" src={border} />
+                                            <img className="sidearrow ones_ar" src={sidearrow} />
+                                            <div> <img className="ques_image" src={question} />
+                                                <div className="button_container">
+                                                    <button> Evolve <span> <IoIosArrowForward /> </span> </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+
                                 </div>
 
 
-                                <div className="evolve_process">
-                                    <div className="border_main_div">
-                                        <h4>Start evolve process</h4>
-                                        <div></div>
-                                        <div></div>
-                                        <div></div>
-                                        <div></div>
-                                    </div>
-                                    <div>
-                                        <img src={question} />
+                            </div>}
+                        {active === "3" &&
+                            <div className="box_mainDiv">
+                                <div className="box_textDiv">
+                                    <div className="button_divMa">
+                                        <div>
+                                            <h4 className="box_nftText">MY NFT GALACTIC GORILLAS</h4>
+                                        </div>
+                                        <div className="both_button">
+                                            <div className="buttons_container">
+                                                <button> CONNECT WALLET <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+
+                                            <div className="buttons_container">
+                                                <button> BUY MONKEY <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+                                        </div>
 
                                     </div>
-                                </div>      </div>
 
 
-                        </div>
+
+                                    <div className="first_textDiv">
+                                        <p className="order_text">The <span className="gal">Alien Gorilla</span>is the <span className="gal">KEY</span> o winning against the alien invader and winning The Inter-Galactic War, you’ll have to sacrifice
+                                            <span className="gal">3 Galactic Gorillas</span> in order to achieve such a strong being and protect the species from extinction.
+                                        </p>
+                                        <p className="note_text"><span className="gal">NOTE:</span> By clicking “<span className="gal">Evolve</span>”, the <span>Galactic Gorillas</span> you’ve chosen will be sent to a Solana black hole wallet and disappear forever, however,
+                                            black hole wallet and disappear forever, however,
+                                            <span>EACH EVOLUTION</span> will make you more money in the <span className="gal">Mooning Monkey Game.</span></p>
+                                    </div>
+
+
+                                    <div className="evolve_process">
+                                        <div className="border_main_div">
+                                            <h4 className="strt_evolve">Start evolve process</h4>
+                                        </div>
+
+                                    </div>
+                                    <div className="round_divTwo">
+                                        <img className="border_img1" src={border} />
+                                        <img className="border_img1 doted" src={border} />
+                                        <img className="border_img1 doted" src={border} />
+                                        <img className="sidearrow1 doted" src={sidearrow} />
+                                        <div>
+                                            <img className="ques_image3" src={question} />
+                                            <div className="buttonss_container">
+                                                <button> Evolve <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+
+                                </div>
+
+
+
+                            </div>
+                        }
+                        {active === "4" &&
+                            <div className="box_mainDiv">
+                                <div className="box_textDiv">
+                                    <div className="button_divMa">
+                                        <div>
+                                            <h4 className="box_nftText">MY NFT ALIEN GORILLAS</h4>
+                                        </div>
+                                        <div className="both_button">
+                                            <div className="buttons_container">
+                                                <button> CONNECT WALLET <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+
+                                            <div className="buttons_container">
+                                                <button> BUY MONKEY <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+
+                                    <div className="first_textDiv">
+                                        <p className="order_text">
+                                            Although <span className="gal">The Alien Gorilla</span> is extremely powerful, it is still bound by the shackles of mortality… as well as an
+                                            unsettling look that’s not easy on the eye, in order to guarantee the survival of the <span className="gal">Mooning Monkey’s Species AND</span> make sure they do so with style… You’ll need to sacrifice<span className="gal"> 2 Of Your Alien Gorillas</span> to make <span className="gal">
+                                                1 Eternal Yeti</span> that will live forever on the Blockchain and earn the highest possible levels of daily game rewards.
+                                        </p>
+                                        <p className="note_text"><span className="gal">NOTE:</span> By clicking “<span className="gal">Evolve</span>”, the <span>Alien Gorillas</span> you’ve chosen will be sent to a Solana black hole wallet and disappear forever, however,
+                                            black hole wallet and disappear forever, however,
+                                            <span className="gal">EACH EVOLUTION</span> will make you more money in the <span className="gal">Mooning Monkey Game.</span></p>
+                                    </div>
+
+
+                                    <div className="evolve_process">
+                                        <div className="border_main_div">
+                                            <h4 className="strt_evolve">Start evolve process</h4>
+
+                                        </div>
+
+
+                                    </div>
+
+                                    <div className="images_divFolder">
+                                        <img className="border_img2" src={border} />
+                                        <img className="border_img2 brrr left_im" src={border} />
+                                        <img className="sidearrow1 arr" src={sidearrow} />
+                                        <img className="sidearrow1 arrs" src={sidearrow} />
+
+
+                                        <div>
+                                            <img className="ques_image1" src={question} />
+                                            <div className="buttonsss_container">
+                                                <button> Evolve <span> <IoIosArrowForward /> </span> </button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+                        }
+
+
+
+
+
+
+
+
 
                     </div>
                 </section>
