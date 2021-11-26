eb1ee7ee9838d329d2b10b85ddfcb8c6da3cbd8f
Junaid Latif <junaidlatif004@gmail.com>
2021-11-26 18:26:18 +0500

    Conflict Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Calculator from "./Components/calculator/calculator";
 import Header from "./Components/Header/Header";
@@ -18,19 +18,11 @@ const App = () => {
   return (
     <>
       <Header />
-<<<<<<< HEAD
-        <Switch>
-          <Route exact path="/" component={Calculator} />
-          <Route path="/token" component={Token} />
-          <Route path="/comic" component={Comic} />
-        </Switch>
-=======
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/token" component={Token} />
         <Route path="/calculator" component={Calculator} />
       </Switch>
->>>>>>> bb9a62839d0049ab90c7ea7c8eefc8714fee8d87
       <Footer />
     </>
   );
