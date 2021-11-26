082a91ec9c568413d93cf6f11b0561a4150d746f
unknown <sp17bsed@gmail.com>
2021-11-26 15:15:31 +0500

    resolve conbflicts

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Calculator from "./Components/calculator/calculator";
 import Header from "./Components/Header/Header";
 import Token from "./Components/Token/Token";
 import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home"
+import Home from "./Components/Home/Home";
 
 import "./App.scss";
 
@@ -20,6 +20,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/token" component={Token} />
+        <Route path="/calculator" component={Calculator} />
       </Switch>
       <Footer />
     </>
